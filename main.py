a6fd9f0f2801b978c4f25226ce8c1f4fb3a90791
Jingkai He <jingkai@hey.com>
2024-09-08 12:36:33 +0000

    more cli options
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- main.py
+++ main.py
@@ -10,13 +10,29 @@ from libs.core.contexts import cli_ctx, react_ctx
 import click
 
 
-@click.command()
+@click.group()
+def opsmate_cli():
+    """
+    OpsMate is an SRE AI assistant that helps you manage production environment.
+    This is the cli tool to interact with OpsMate.
+    """
+    pass
+
+
+@opsmate_cli.command()
 @click.argument("instruction")
 @click.option(
     "--ask", is_flag=True, help="Ask for confirmation before executing commands"
 )
-@click.option("--model", default="gpt-4o", help="OpenAI model to use")
-def main(instruction, ask, model):
+@click.option(
+    "--model",
+    default="gpt-4o",
+    help="OpenAI model to use. To list models available please run the list-models command.",
+)
+def ask(instruction, ask, model):
+    """
+    Ask a question to the OpsMate.
+    """
     task = Task(
         metadata=Metadata(
             name=instruction,
@@ -33,5 +49,18 @@ def main(instruction, ask, model):
     print(exec_react_task(OpenAI(), task, ask=ask, model=model))
 
 
+@opsmate_cli.command()
+def list_models():
+    """
+    List all the models available in OpenAI.
+    """
+    client = OpenAI()
+    model_names = [
+        model.id for model in client.models.list().data if model.id.startswith("gpt")
+    ]
+    for model_name in model_names:
+        print(model_name)
+
+
 if __name__ == "__main__":
-    main()
+    opsmate_cli()

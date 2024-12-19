a052a63792c62fb20098a04d6758055a9381afd2
Jingkai He <jingkai@hey.com>
2024-12-19 11:26:03 +0000

    support react
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- sugar.py
+++ sugar.py
@@ -1,7 +1,7 @@
 import instructor
 from openai import AsyncOpenAI
 from pydantic import BaseModel, Field, create_model
-from typing import Any, Callable, Optional, List, Union, Iterable
+from typing import Any, Callable, Optional, List, Union, Iterable, Coroutine
 from functools import wraps
 import asyncio
 import inspect
@@ -73,7 +73,10 @@ def dino(
                 )
                 for resp in initial_response:
                     if isinstance(resp, BaseModel):
-                        resp()
+                        if inspect.iscoroutinefunction(resp.__call__):
+                            await resp.__call__()
+                        else:
+                            resp.__call__()
                         messages.append(
                             {"role": "user", "content": resp.model_dump_json()}
                         )
@@ -91,7 +94,7 @@ def dino(
     return wrapper
 
 
-def dtool(fn: Callable):
+def dtool(fn: Callable | Coroutine[Any, Any, Any]):
     """
     dtool is a decorator that turns a function into a Pydantic model.
 
@@ -123,20 +126,38 @@ def dtool(fn: Callable):
     m = create_model(fn.__name__, **kw)
 
     # patch the __call__ method
-    def call(self):
-        s = self.model_dump()
-        s.pop("output")
-        self.output = fn(**s)
-        return self.output
+    if inspect.iscoroutinefunction(fn):
+
+        async def call(self):
+            s = self.model_dump()
+            s.pop("output")
+            self.output = await fn(**s)
+            return self.output
+
+    else:
+
+        def call(self):
+            s = self.model_dump()
+            s.pop("output")
+            self.output = fn(**s)
+            return self.output
 
     m.__call__ = call
 
     return m
 
 
-class UserInfo(BaseModel):
-    name: str = Field(description="The name of the user")
-    email: str = Field(description="The email of the user")
+class React(BaseModel):
+    thoughts: str = Field(description="Your thought about the question")
+    action: str = Field(description="Action to take based on your thoughts")
+
+
+class ReactAnswer(BaseModel):
+    answer: str = Field(description="Your final answer to the question")
+
+
+class Observation(BaseModel):
+    observation: str = Field(description="The observation of the action")
 
 
 import subprocess
@@ -165,19 +186,81 @@ class ShellCommand(BaseModel):
         return self.output
 
 
+@dino("gpt-4o", response_model=Union[React, ReactAnswer])
+async def react(question: str, context: List[Message]):
+    """
+    <assistant>
+    You run in a loop of question, thought, action.
+    At the end of the loop you output an answer.
+    Use "Question" to describe the question you have been asked.
+    Use "Thought" to describe your thought
+    Use "Action" to describe the action you are going to take based on the thought.
+    Use "Answer" as the final answer to the question.
+    </assistant>
+
+    <response format 1>
+    During the thought phase you response with the following format:
+    thought: ...
+    action: ...
+    </response_format 1>
+
+    <response format 2>
+    When you have an answer, you response with the following format:
+    answer: ...
+    </response_format 2>
+    """
+
+    return [
+        Message(role="user", content=question),
+        *context,
+    ]
+
+
+@dtool
+def run_command(command: str) -> str:
+    return ShellCommand(command=command)()
+
+
+async def run_react(
+    question: str,
+    pretext: str = "",
+    tools: List[BaseModel] = [],
+):
+
+    @dino("gpt-4o-mini", response_model=Observation, tools=tools)
+    async def run_action(react: React):
+        return [
+            Message(role="system", content=pretext),
+            Message(role="assistant", content=react.model_dump_json()),
+        ]
+
+    context = []
+    if pretext:
+        context.append(Message(role="system", content=pretext))
+    while True:
+        react_result = await react(question, context)
+        if isinstance(react_result, React):
+            context.append(
+                Message(role="assistant", content=react_result.model_dump_json())
+            )
+            yield react_result
+            observation = await run_action(react_result)
+            context.append(
+                Message(role="assistant", content=observation.model_dump_json())
+            )
+            yield observation
+        elif isinstance(react_result, ReactAnswer):
+            yield react_result
+            break
+
+
 @dino("gpt-4o-mini", response_model=str)
 async def say_hello(name: str):
     return f"just say hi to {name}"
 
 
-### Information Extraction example
-@dino("gpt-4o-mini", response_model=UserInfo)
-async def get_user_info(text: str):
-    return f"extract the username and email from {text}"
-
-
 ### Abitrary Tool calling example
-@dino("gpt-4o", response_model=ShellCommand)
+@dino("gpt-4o-mini", response_model=ShellCommand)
 async def run_shell_command(instruction: str):
     """
     You are a world class sysadmin who is good at running shell commands.
@@ -193,7 +276,7 @@ async def run_shell_command(instruction: str):
 
 ### function calling example
 @dtool
-def birthday_lookup(person_name: str) -> str:
+async def birthday_lookup(person_name: str) -> str:
     """
     The function looks up the birthday of the person.
     """
@@ -212,24 +295,57 @@ async def get_birthday(people: str):
     return f"find out the birthday of {people}"
 
 
+class UserInfo(BaseModel):
+    name: str = Field(description="The name of the user")
+    email: str = Field(description="The email of the user")
+
+
+### Information Extraction example
+@dino("gpt-4o-mini", response_model=UserInfo)
+async def get_user_info(text: str):
+    return f"extract the username and email from {text}"
+
+
+@dtool
+@dino("gpt-4o-mini", response_model=str)
+async def alice(instruction: str) -> str:
+    """
+    Your name is Alice, you are a personal assistant.
+    """
+    return "please do the following: " + instruction
+
+
+@dino("gpt-4o-mini", response_model=str, tools=[alice])
+async def bob(instruction: str):
+    return "delegate the following instruction: " + instruction
+
+
 async def main():
-    hello = await say_hello("Jingkai He")
-    print(hello)
+    # result = await bob("tell a joke that mentions your name")
+    # print(result)
 
-    user_info = await get_user_info(
-        "User Jingkai He has an email jingkai.he@example.com"
-    )
+    # user_info = await get_user_info(
+    #     "User Jingkai He has an email jingkai.he@example.com"
+    # )
 
-    assert user_info.name == "Jingkai He"
-    assert user_info.email == "jingkai.he@example.com"
+    # assert user_info.name == "Jingkai He"
+    # assert user_info.email == "jingkai.he@example.com"
 
-    shell_command = await run_shell_command("what's the operating system?")
-    print(shell_command().lower())
-    assert "linux" in shell_command().lower()
+    # hello = await say_hello("Jingkai He")
+    # print(hello)
 
-    birthday = await get_birthday("Jingkai He and Boris Johnson")
+    # shell_command = await run_shell_command("what's the operating system?")
+    # print(shell_command().lower())
 
-    print(birthday)
+    # birthday = await get_birthday("Jingkai He and Boris Johnson")
+    # print(birthday)
+
+    async for result in run_react(
+        "how many pods are in the cluster?",
+        pretext="you have kubectl command to run",
+        tools=[run_command],
+    ):
+        print(result)
 
 
 if __name__ == "__main__":

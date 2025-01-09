4666552e9e34e96f56642b9ec4a685e4749b20c6
Jingkai He <jingkai@hey.com>
2025-01-09 20:51:16 +0000

    bump version to 0.1.10.alpha
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- cli/sdk/configuration.go
+++ cli/sdk/configuration.go
@@ -86,7 +86,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0.1.9.alpha1/go",
+		UserAgent:        "OpenAPI-Generator/0.1.10.alpha/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{

98b4d7f53f9648f1829dedf40e91c56c28ba46d0
Jingkai He <jingkai@hey.com>
2025-01-15 22:03:11 +0000

    bump version to 0.1.12.alpha2
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- sdk/python/opsmatesdk/configuration.py
+++ sdk/python/opsmatesdk/configuration.py
@@ -494,7 +494,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1.0\n"\
-               "SDK Package Version: 0.1.12.alpha".\
+               "SDK Package Version: 0.1.12.alpha2".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:

fbbed7ebb5368746da6a6758c025cfefe1d589ad
Jingkai He <jingkai@hey.com>
2025-01-23 20:38:46 +0000

    ensure proper context population for the task execution
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- opsmate/polya/execution.py
+++ opsmate/polya/execution.py
@@ -9,40 +9,43 @@ import yaml
 logger = structlog.get_logger(__name__)
 
 
+iac_sme_context = """
+<rule 1>
+Before making any changes, you must read the file(s) to understand:
+* the purpose of the file (e.g. a terraform file deploying IaC, or a yaml file deploying k8s resources)
+* Have a basic understanding of the file's structure
+</rule 1>
+
+<rule 2>
+Edit must be precise and specific:
+* Tabs and spaces must be used correctly
+* The line range must be specified when you are performing an update operation against a file
+* Stick to the task you are given, don't make drive-by changes
+</rule 2>
+
+<rule 3>
+After you make the change, you must verify the updated content is correct using the `ACITool.view` or `ACITool.search` commands.
+</rule 3>
+
+<rule 4>
+Tool usage:
+* `ACITool` tool for file search, view, create, update, append and undo.
+* `ShellCommand` tool for running shell commands that cannot be covered by `ACITool`, in other words, do not use `ShellCommand` to run `view/vim/emacs/nano/vi` commands.
+* `SysChdir` tool for changing the current working directory, **DO NOT** use `cd` command.
+</rule 4>
+"""
+
+
 @react(
     model="claude-3-5-sonnet-20241022",
     tools=[ACITool, ShellCommand, GithubCloneAndCD, SysChdir],
-    contexts=["you are an SRE who is tasked to modify the infra as code"],
+    contexts=[iac_sme_context],
     tool_calls_per_action=1,
     iterable=True,
 )
 async def iac_sme(instruction: str):
     """
     You are an SRE who is tasked to modify the infra as code.
-
-    <rule 1>
-    Before making any changes, you must read the file(s) to understand:
-    * the purpose of the file (e.g. a terraform file deploying IaC, or a yaml file deploying k8s resources)
-    * Have a basic understanding of the file's structure
-    </rule 1>
-
-    <rule 2>
-    Edit must be precise and specific:
-    * Tabs and spaces must be used correctly
-    * The line range must be specified when you are performing an update operation against a file
-    * Stick to the task you are given, don't make drive-by changes
-    </rule 2>
-
-    <rule 3>
-    After you make the change, you must verify the updated content is correct using the `ACITool.view` or `ACITool.search` commands.
-    </rule 3>
-
-    <rule 4>
-    Tool usage:
-    * `ACITool` tool for file search, view, create, update, append and undo.
-    * `ShellCommand` tool for running shell commands that cannot be covered by `ACITool`, in other words, do not use `ShellCommand` to run `view/vim/emacs/nano/vi` commands.
-    * `SysChdir` tool for changing the current working directory, DO NOT use `cd` command.
-    </rule 4>
     """
     return instruction
 

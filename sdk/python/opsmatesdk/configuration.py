784a67b0227ef8827165dd664c33197ebcc155b8
Jingkai He <jingkai@hey.com>
2025-03-02 15:03:24 +0000

    bump version to 0.1.30a0
    Signed-off-by: Jingkai He <jingkai@hey.com>

--- sdk/python/opsmatesdk/configuration.py
+++ sdk/python/opsmatesdk/configuration.py
@@ -494,7 +494,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 0.1.0\n"\
-               "SDK Package Version: 0.1.29a0".\
+               "SDK Package Version: 0.1.30a0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:

ad0e0c76e529935d7f81e326fb227b48bfff18c8
Jingkai He <jingkai@hey.com>
2025-03-31 09:42:24 +0000

    break down runtime to multiple files
    Signed-off-by: Jingkai He <jingkai@hey.com>

---
+++ opsmate/runtime/__init__.py
@@ -0,0 +1,4 @@
+from opsmate.runtime.runtime import Runtime, RuntimeError
+from opsmate.runtime.local import LocalRuntime
+
+__all__ = ["Runtime", "LocalRuntime", "RuntimeError"]
--- opsmate/tests/runtime/test_runtime.py
+++ opsmate/tests/runtime/test_runtime.py
@@ -1,6 +1,6 @@
 import pytest
 import asyncio
-from opsmate.runtime.runtime import LocalRuntime, RuntimeError
+from opsmate.runtime import LocalRuntime, RuntimeError
 from contextlib import asynccontextmanager
 
 
